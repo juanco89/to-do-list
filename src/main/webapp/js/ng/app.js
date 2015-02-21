c737b9b901b8a62f844fe8dde8b61f97fd6541ee
Juan C. Orozco <juanco89@gmail.com>
2015-02-20 23:45:17 -0500

    Se usa el recurso Tarea para buscar la lista actual de tareas.
    Se muestra la lista consultada con resource, ahora usando el $scope.
    Se realiza upgrade de versiones angular.

--- src/main/webapp/js/ng/app.js
+++ src/main/webapp/js/ng/app.js
@@ -2,21 +2,44 @@
 (function(){
 	var app = angular.module('AppToDo', ['ngResource']);
 	
-	app.controller('TasksController', ['$http', function($http){
+	app.service('ListaTareas', function() {
+
+		var tasks = [];
+
+		this.setTasks = function(collection) {
+			tasks = collection;
+		};
+
+		this.addTask = function(t) {
+			tasks.unshift(t);
+		};
+
+		this.getTasks = function(){
+			return tasks;
+		};
+	});
+
+	app.factory('Tarea', function($resource) {
+
+		return $resource('/to-do-list/rest/tasks/:id');
+
+	});
+
+	app.controller('TasksController', ['$scope', '$http', 'Tarea', 'ListaTareas', function($scope, $http, Tarea, ListaTareas){
 		
-		var tareas = this;
-		tareas.tareas = [];
-		
-		$http.get('/to-do-list/rest/tareas/buscarTodas').success(function(data) {
-			tareas.tareas = data;
-			allTasks = tareas.tareas;
-			
-		}).error(function(error) {
-			alert(error);
+$scope.hola ='Hola mundo';
+
+		Tarea.query(function(value) {
+			ListaTareas.setTasks(value);
 		});
+
+		$scope.tasks = function() {
+			return ListaTareas.getTasks();
+		};
+
 	}]);
-	
-	app.controller('FormController', function(){
+
+	app.controller('FormController',[ '$scope', 'Tarea', 'ListaTareas' ,function($scope, Tarea, ListaTareas){
 		this.tarea = {};
 		
 		var initTask = function() {
@@ -33,7 +56,7 @@
 			this.tarea = {};
 			initTask();
 		};
-	});
+	}]);
 	
 	
 	var allTasks = [];
